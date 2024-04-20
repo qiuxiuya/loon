ca1182bbb0017e7dffe56a05eafa2ccbb0260d74
qiuxiuya <95994462+qiuxiuya@users.noreply.github.com>
2024-04-21 07:59:09 +0800

    Delete VPS/bingcrt.sh

--- VPS/bingcrt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-YELLOW='\033[1;33m'
-RESET='\033[0m'
-rm -r /root/crt
-mkdir -p /root/crt
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /root/crt/bing.key -out /root/crt/bing.crt -subj "/C=US/ST=YourState/L=YourCity/O=YourOrganization/CN=www.bing.com" > /dev/null 2>&1
-echo -e "${YELLOW}已生成bing自签证书\n证书公钥在: /root/crt/bing.crt \n证书私钥在: /root/crt/bing.key${RESET}"
