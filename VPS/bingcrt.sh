664a4e72c4031c83ffcf48aed54440fa8c7b17fe
qiuxiuya <95994462+qiuxiuya@users.noreply.github.com>
2024-06-06 16:40:07 +0800

    Update bingcrt.sh

--- VPS/bingcrt.sh
+++ VPS/bingcrt.sh
@@ -2,8 +2,8 @@
 YELLOW='\033[1;33m'
 RESET='\033[0m'
 
-rm -rf /root/crt
-mkdir -p /root/crt
+rm -rf /root/ssl
+mkdir -p /root/ssl
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /root/crt/bing.key -out /root/crt/bing.crt -subj "/C=US/ST=YourState/L=YourCity/O=YourOrganization/CN=www.bing.com" > /dev/null 2>&1
-echo -e "${YELLOW}已生成bing自签证书\n证书公钥在: /root/crt/bing.crt \n证书私钥在: /root/crt/bing.key${RESET}"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /root/ssl/bing.key -out /root/ssl/bing.crt -subj "/C=US/ST=YourState/L=YourCity/O=YourOrganization/CN=www.bing.com" > /dev/null 2>&1
+echo -e "${YELLOW}已生成bing自签证书\n证书公钥在: /root/ssl/bing.crt \n证书私钥在: /root/ssl/bing.key${RESET}"
